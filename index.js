debc616e11db9c4bddfc88df081bffecd447ff83
furrma <annegaba07@yahoo.com>
2019-06-14 01:03:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
 const mysql =require('mysql');
+const express = require('express');
+var app = express();
+const bodyparser = require('body-parser');
+
+app.use(bodyparser.json)
 
 var mysqlConnection = mysql.createConnection({
     host:'localhost',
@@ -12,4 +17,17 @@ mysqlConnection.connect((err)=>{
     console.log('Database connection succeeded.');
     else
     console.log('Database connection failed \n Error: ' + JSON.stringify(err,undefined,2));
+});
+
+
+app.listen(3000,()=>console.log('Express server is running at port number: 3000'));
+
+//get items
+app.get('/items',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM items',(err, rows, field)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
 });
