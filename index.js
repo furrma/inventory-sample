f1466d1cdb80c5d5ab5c057076cb75c59f5d559d
furrma <annegaba07@yahoo.com>
2019-06-14 02:01:15 +0800

    Update index.js
    added retrieve single row, delete, post, and put

--- index.js
+++ index.js
@@ -9,7 +9,8 @@ var mysqlConnection = mysql.createConnection({
     host:'localhost',
     user: 'root',
     password: '',
-    database: 'inventory'
+    database: 'inventory',
+    multipleStatements: true
 });
 
 mysqlConnection.connect((err)=>{
@@ -22,11 +23,60 @@ mysqlConnection.connect((err)=>{
 
 app.listen(3000,()=>console.log('Express server is running at port number: 3000'));
 
-//get items
+//get all items
 app.get('/items',(req, res)=>{
     mysqlConnection.query('SELECT * FROM items',(err, rows, fields)=>{
         if(!err)
         res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+//get item
+app.get('/items/:id',(req, res)=>{
+    mysqlConnection.query('SELECT * FROM items WHERE id = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send(rows);
+        else
+        console.log(err);
+    })
+});
+
+//delete item
+app.delete('/items/:id',(req, res)=>{
+    mysqlConnection.query('DELETE FROM items WHERE id = ?',[req.params.id],(err, rows, fields)=>{
+        if(!err)
+        res.send('Deleted successfully.');
+        else
+        console.log(err);
+    })
+});
+
+//add item
+app.post('/items',(req, res)=>{
+    let emp = req.body;
+    var sql = "SET @id = ?; SET @name = ?; SET @qty = ?; SET @amount = ?; \
+    CALL ItemAddEdit(@id,@name,@qty,@amount);";
+    mysqlConnection.query(sql,[emp.id,emp.name,emp.qty,emp.amount],(err, rows, fields)=>{
+        if(!err)
+            rows.forEach(element => {
+                if(element.constructor == Array)
+                res.send('Inserted id: ' + element[0].id)
+            });
+        else
+        console.log(err);
+    })
+});
+
+//update item
+app.put('/items',(req, res)=>{
+    let emp = req.body;
+    var sql = "SET @id = ?; SET @name = ?; SET @qty = ?; SET @amount = ?; \
+    CALL ItemAddEdit(@id,@name,@qty,@amount);";
+    mysqlConnection.query(sql,[emp.id,emp.name,emp.qty,emp.amount],(err, rows, fields)=>{
+        if(!err)
+            res.send('Updated successfully.')
         else
         console.log(err);
     })
